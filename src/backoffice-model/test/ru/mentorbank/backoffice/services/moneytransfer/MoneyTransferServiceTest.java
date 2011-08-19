6a07de0f8df390d48acfcb6403375b4fc9672d02
U-ITECO\kiritsev <kiritsev@inv2986-pc.msk.i-teco.ru>
2011-08-19 15:58:41 +0400

    Final edition

--- src/backoffice-model/test/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferServiceTest.java
+++ src/backoffice-model/test/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferServiceTest.java
@@ -1,19 +1,19 @@
 package ru.mentorbank.backoffice.services.moneytransfer;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.refEq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import ru.mentorbank.backoffice.dao.OperationDao;
 import ru.mentorbank.backoffice.dao.exception.OperationDaoException;
-import ru.mentorbank.backoffice.model.Operation;
+import ru.mentorbank.backoffice.model.stoplist.JuridicalStopListRequest;
+import ru.mentorbank.backoffice.model.stoplist.StopListInfo;
+import ru.mentorbank.backoffice.model.stoplist.StopListStatus;
 import ru.mentorbank.backoffice.model.transfer.JuridicalAccountInfo;
 import ru.mentorbank.backoffice.model.transfer.TransferRequest;
 import ru.mentorbank.backoffice.services.accounts.AccountService;
@@ -25,33 +25,27 @@ import ru.mentorbank.backoffice.test.AbstractSpringTest;
 
 public class MoneyTransferServiceTest extends AbstractSpringTest {
 
-	@Autowired
+	// @Autowired
 	private MoneyTransferServiceBean moneyTransferService;
+	private StopListServiceStub mockedStopListService;
 	private AccountService mockedAccountService;
-
 	private JuridicalAccountInfo srcAccountInfo;
 	private TransferRequest transferRequest;
 	private JuridicalAccountInfo dstAccountInfo;
 
 	@Before
 	public void setUp() {
+		moneyTransferService = new MoneyTransferServiceBean();
+
 		srcAccountInfo = new JuridicalAccountInfo();
-		srcAccountInfo.setAccountNumber("111111111111111");
 		srcAccountInfo.setInn(StopListServiceStub.INN_FOR_OK_STATUS);
 
 		dstAccountInfo = new JuridicalAccountInfo();
-		dstAccountInfo.setAccountNumber("222222222222222");
 		dstAccountInfo.setInn(StopListServiceStub.INN_FOR_OK_STATUS);
 
 		transferRequest = new TransferRequest();
 		transferRequest.setSrcAccount(srcAccountInfo);
 		transferRequest.setDstAccount(dstAccountInfo);
-
-		mockedAccountService = mock(AccountServiceBean.class);
-		// Dynamic Stub
-		when(mockedAccountService.verifyBalance(dstAccountInfo)).thenReturn(
-				true);
-		moneyTransferService.setAccountService(mockedAccountService);
 
 	}
 
@@ -71,30 +65,49 @@ public class MoneyTransferServiceTest extends AbstractSpringTest {
 		// verify(mockedStopListService).getJuridicalStopListInfo(null);
 		// verify(mockedAccountService).verifyBalance(null);
 
-		// ####################
-		/*
-		 * StopListService mockedStopListService =
-		 * mock(StopListServiceStub.class);
-		 * moneyTransferService.setStopListService(mockedStopListService);
-		 */
-		OperationDao operationDao = new OperationsSet();
-		moneyTransferService.setOperationDao(operationDao);
+		setStopListService();
 
-		assertTrue(operationDao.getOperations().isEmpty());
+		setAccountService();
+
+		setOperationDao();
 
 		moneyTransferService.transfer(transferRequest);
 
-		assertFalse(operationDao.getOperations().isEmpty());
-		assertThat(operationDao.getOperations().size(), is(1));
+		JuridicalStopListRequest request2 = new JuridicalStopListRequest();
+		request2.setInn(srcAccountInfo.getInn());
 
-		Operation ops[] = {};
-		ops = operationDao.getOperations().toArray(ops);
-		assertThat(ops[0].getSrcAccount().getAccountNumber(),
-				is(srcAccountInfo.getAccountNumber()));
-		assertThat(ops[0].getDstAccount().getAccountNumber(),
-				is(dstAccountInfo.getAccountNumber()));
+		verify(mockedStopListService, times(2)).getJuridicalStopListInfo(
+				refEq(request2));
+		verify(mockedAccountService).verifyBalance(dstAccountInfo);
+	}
 
-		// verify(mockedStopListService).getJuridicalStopListInfo(null);
-		// verify(mockedAccountService).verifyBalance(null);
+	private void setStopListService() {
+		mockedStopListService = mock(StopListServiceStub.class);
+		JuridicalStopListRequest request = new JuridicalStopListRequest();
+		request.setInn(srcAccountInfo.getInn());
+		JuridicalStopListRequest request1 = new JuridicalStopListRequest();
+		request1.setInn(dstAccountInfo.getInn());
+
+		StopListInfo stopListInfo_OK = new StopListInfo();
+		stopListInfo_OK.setStatus(StopListStatus.OK);
+
+		when(mockedStopListService.getJuridicalStopListInfo(refEq(request)))
+				.thenReturn(stopListInfo_OK);
+		when(mockedStopListService.getJuridicalStopListInfo(refEq(request1)))
+				.thenReturn(stopListInfo_OK);
+
+		moneyTransferService.setStopListService(mockedStopListService);
+	}
+
+	private void setAccountService() {
+		mockedAccountService = mock(AccountServiceBean.class);
+		when(mockedAccountService.verifyBalance(dstAccountInfo)).thenReturn(
+				true);
+		moneyTransferService.setAccountService(mockedAccountService);
+	}
+
+	private void setOperationDao() {
+		OperationDao operationDao = new OperationsSet();
+		moneyTransferService.setOperationDao(operationDao);
 	}
 }
